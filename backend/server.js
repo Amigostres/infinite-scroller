093a5101fdb18ba5f46104d67c0e46f18d749d3e
Jose Martinez <josemanuel21302@gmail.com>
2024-06-19 04:06:14 -0700

    server now sends the respective photo details and now cors does not interfere anymore

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,32 @@ require('dotenv').config()
 
 
 const app = express()
+app.use(cors())
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json('hi')
 })
 
-app.get('/api', (req, res) => {
-    
+app.get('/api', async (req, res) => {
+    console.log('api use requested');
+    const count = 1
+    const options = {
+        method: "GET",
+        url: `https://api.unsplash.com/photos/random/?client_id=${process.env.UNPLASH_API_KEY}&count=${count}`
+    }
+
+
+    try {
+        console.log('trying to find data');
+        const response = await axios.request(options);
+        console.log('data sent!');
+        res.json(response.data); // Send only the data to the client
+    } catch (e) {
+        console.log(e);
+    }
 })
+
 
 
 app.listen(port, () => {
